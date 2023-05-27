8e871fc1631ca3e847aba86c8a99957fad0003e9
Brenda Pereira <brendacamara@outlook.com>
2023-05-27 05:59:28 -0300

    feat: create symtoms page

---
+++ frontend/src/pages/symptomsPage/symptomsPage.tsx
@@ -0,0 +1,11 @@
+import { Header } from '@/components/header/Header';
+import styles from './symptomsPage.module.scss';
+
+export const SymptomsPage = () => {
+  return (
+    <div className={styles.wrapper}>
+      <Header />
+      <div>teste</div>
+    </div>
+  );
+};
