6e90b65144868e3d419cd75f1cb9bf45ec1a21a0
Lázaro <lazaromacielv@gmail.com>
2023-05-27 15:57:07 -0300

    Removing openai token and adjust rule of broke lines

--- backend/src/controllers/DiagnosticController.ts
+++ backend/src/controllers/DiagnosticController.ts
@@ -20,7 +20,7 @@ class DiagnosticController {
           model: "gpt-3.5-turbo",
           messages: [
             {"role": "system", "content": "Você irá ajudar um médico que quer uma segunda opinião para seu diagnostico, ele esta ciente de que não pode confiar"},
-            {"role": "user", "content": "Sou um médico e preciso de uma opinião sua sobre meu diagnostico, gostaria da resposta sem quebras de linha"},
+            {"role": "user", "content": "Sou um médico e preciso de uma opinião sua sobre meu diagnostico"},
             {"role": "user", "content": userQuestion }
           ],
         });
