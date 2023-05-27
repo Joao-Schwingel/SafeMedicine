e671ee0fa463737534417fb23043ee4fa330f1ed
Lázaro <lazaromacielv@gmail.com>
2023-05-27 15:01:14 -0300

    Removing token

--- backend/src/config/constants.ts
+++ backend/src/config/constants.ts
@@ -1,4 +1,4 @@
 export const API_PORT = parseInt(process.env.API_PORT || '8081');
-export const OPENAI_TOKEN ='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';
+export const OPENAI_TOKEN ='sk-7W9EgKAxa62pmktnajyR3BlbkFJgD1Y4X216BtP3PF8HdGz';
 
 
