cd677461407e163299a9a9db16d81b024ffa0523
Lázaro <lazaromacielv@gmail.com>
2023-05-27 15:22:53 -0300

    Inserting token on dotenv

--- backend/src/config/constants.ts
+++ backend/src/config/constants.ts
@@ -1,4 +1,5 @@
+require('dotenv').config()
 export const API_PORT = parseInt(process.env.API_PORT || '8081');
-export const OPENAI_TOKEN ='sk-7W9EgKAxa62pmktnajyR3BlbkFJgD1Y4X216BtP3PF8HdGz';
+export const OPENAI_TOKEN = process.env.OPENAI_TOKEN;
 
 
